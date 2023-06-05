51a1484fb8860da1d930f4bbbdbe4a1be4e83403
tkadziolka <tmax0135@gmail.com>
2023-06-05 18:06:04 +0200

    Created test for punctuation location

--- src/main/java/dev/snipme/highlights/internal/locator/PunctuationLocator.kt
+++ src/main/java/dev/snipme/highlights/internal/locator/PunctuationLocator.kt
@@ -2,19 +2,24 @@ package dev.snipme.highlights.internal.locator
 
 import dev.snipme.highlights.model.PhraseLocation
 import dev.snipme.highlights.internal.SyntaxTokens.PUNCTUATION_CHARACTERS
+import dev.snipme.highlights.internal.SyntaxTokens.TOKEN_DELIMITERS
 import dev.snipme.highlights.internal.indicesOf
 
 internal object PunctuationLocator {
     fun locate(code: String): List<PhraseLocation> {
-        val locations = mutableListOf<PhraseLocation>()
+        val locations = mutableSetOf<PhraseLocation>()
         code.asSequence()
-            .toSet()
-            .filter { it.toString() in PUNCTUATION_CHARACTERS }
+            .map { it.toString().trim() }
+            .filter { it in TOKEN_DELIMITERS }
+            .filter { it.isNotBlank() }
+            .filter { it in PUNCTUATION_CHARACTERS }
             .forEach {
-                code.indicesOf(it.toString()).forEach { index ->
+                code.indicesOf(it).forEach { index ->
+                    if (code[index].isWhitespace()) return@forEach
                     locations.add(PhraseLocation(index, index + 1))
                 }
             }
-        return locations
+
+        return locations.toList()
     }
 }
---
+++ src/test/java/dev/snipme/highlights/internal/locator/PunctuationLocatorTest.kt
@@ -0,0 +1,67 @@
+package dev.snipme.highlights.internal.locator
+
+import dev.snipme.highlights.internal.printResults
+import dev.snipme.highlights.model.PhraseLocation
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+internal class PunctuationLocatorTest {
+
+    @Test
+    fun `Returns location of punctuation characters`() {
+        val testCode = """
+            , .  :  ;
+        """.trimIndent()
+
+        val result = PunctuationLocator.locate(testCode)
+
+        assertEquals(4, result.size)
+        assertEquals(PhraseLocation(0, 1), result[0])
+        assertEquals(PhraseLocation(2, 3), result[1])
+        assertEquals(PhraseLocation(5, 6), result[2])
+        assertEquals(PhraseLocation(8, 9), result[3])
+    }
+
+    @Test
+    fun `Returns multiple locations of the same punctuation`() {
+        val testCode = """
+            , ); ),
+        """.trimIndent()
+
+        val result = PunctuationLocator.locate(testCode)
+
+        assertEquals(3, result.size)
+        assertEquals(PhraseLocation(0, 1), result[0])
+        assertEquals(PhraseLocation(6, 7), result[1])
+        assertEquals(PhraseLocation(3, 4), result[2])
+    }
+
+    @Test
+    fun `Returns locations of the punctuation next to each other`() {
+        val testCode = """
+            ,,,
+        """.trimIndent()
+
+        val result = PunctuationLocator.locate(testCode)
+
+        assertEquals(3, result.size)
+        assertEquals(PhraseLocation(0, 1), result[0])
+        assertEquals(PhraseLocation(1, 2), result[1])
+        assertEquals(PhraseLocation(2, 3), result[2])
+    }
+
+    @Test
+    fun `Returns locations of the punctuation next between tokens`() {
+        val testCode = """
+            ,,,class ,
+        """.trimIndent()
+
+        val result = PunctuationLocator.locate(testCode)
+
+        assertEquals(4, result.size)
+        assertEquals(PhraseLocation(0, 1), result[0])
+        assertEquals(PhraseLocation(1, 2), result[1])
+        assertEquals(PhraseLocation(2, 3), result[2])
+        assertEquals(PhraseLocation(9, 10), result[3])
+    }
+}
