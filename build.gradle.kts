97a71307873595fb0e306235ba607911cc1453f9
tkadziolka <tmax0135@gmail.com>
2023-06-24 10:55:12 +0200

    Added signing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.gradle.api.publish.maven.MavenPublication
+import org.jetbrains.kotlin.gradle.plugin.mpp.pm20.util.archivesName
 
 apply(from = "publish-root.gradle")
 
@@ -8,6 +9,7 @@ plugins {
     kotlin("jvm") version "1.8.21"
     id("maven-publish")
     id("io.github.gradle-nexus.publish-plugin") version "1.3.0"
+    id("signing")
 }
 
 val libraryName = "Highlights"
@@ -19,7 +21,9 @@ version = "0.1.0-SNAPSHOT"
 publishing {
     publications {
         create<MavenPublication>("HighlightsPublication") {
-            from(components["java"])
+
+            from(components["java"]) // jar
+
             groupId = group as String
             artifactId = libraryName
             version = version as String
@@ -27,13 +31,15 @@ publishing {
             pom {
                 name.set(libraryName)
                 description.set(libraryDescription)
-                url.set("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+                url.set("https://github.com/SnipMeDev/Highlights")
+
                 licenses {
                     license {
                         name.set("Apache-2.0")
                         url.set("https://opensource.org/license/apache-2-0")
                     }
                 }
+
                 developers {
                     developer {
                         id.set("tkadziolka")
@@ -48,6 +54,15 @@ publishing {
     repositories {
         mavenLocal()
     }
+}
+
+signing {
+    useInMemoryPgpKeys(
+        rootProject.ext["signing.keyId"] as String,
+        rootProject.ext["signing.key"] as String,
+        rootProject.ext["signing.password"] as String
+    )
+    sign(publishing.publications)
 }
 
 java {
