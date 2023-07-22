d42ac81411b234424c8acbfc4d87567fcf22f49c
tkadziolka <tmax0135@gmail.com>
2023-07-22 15:33:23 +0200

    Created sample folder with library usage

---
+++ sample/build.gradle.kts
@@ -0,0 +1,34 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.8.21"
+    application
+}
+
+group = "dev.snipme"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClass.set("MainKt")
+}
+
+dependencies {
+    implementation("dev.snipme:highlights:0.3.0-SNAPSHOT")
+}
---
+++ sample/src/main/kotlin/Main.kt
@@ -0,0 +1,69 @@
+import dev.snipme.highlights.Highlights
+import dev.snipme.highlights.model.BoldHighlight
+import dev.snipme.highlights.model.PhraseLocation
+import dev.snipme.highlights.model.SyntaxLanguage
+
+val sampleClass = """
+    @Serializable
+    public class ExampleClass {
+        // Single-line comment
+        
+        /* Multi-line
+         * comment */
+        
+        // Class variables
+        private int x;
+        protected float y;
+        public static final String MESSAGE = "Hello!";
+        
+        // Constructor
+        public ExampleClass() {
+            this.x = 0;
+            this.y = 0.0f;
+        }
+        
+        // Method with parameters and return type
+        public static int calculateSum(int a, int b) {
+            int sum = a + b;
+            return sum;
+        }
+    }
+""".trimIndent()
+
+fun main(args: Array<String>) {
+    println("### HIGHLIGHTS ###")
+    println()
+    println("Available languages:")
+    println("${SyntaxLanguage.getNames()}")
+    println()
+    println("Available themes:")
+    println("${Highlights.themes(darkMode = false).keys}")
+    println()
+    println("This is a sample class:")
+    println(sampleClass)
+    println()
+
+    val highlights = Highlights.Builder()
+        .code(sampleClass)
+        .language(SyntaxLanguage.JAVA)
+        .build()
+
+    val structure = highlights.getCodeStructure()
+
+    println("After analysis there has been found:")
+    println("${structure.printPhrases(sampleClass)}")
+    println()
+
+    val newInstance = highlights.getBuilder()
+        .emphasis(PhraseLocation(0, 13))
+        .build()
+
+    println("The emphasis was put on the word:")
+    val emphasisLocation = newInstance
+        .getHighlights()
+        .filterIsInstance<BoldHighlight>()
+        .first()
+        .location
+
+    println(sampleClass.substring(emphasisLocation.start, emphasisLocation.end))
+}
