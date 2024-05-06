d605099cd6d7b52d7a91e6450420a9f9aeff3caa
tkadziolka <tmax0135@gmail.com>
2024-05-06 07:25:12 +0200

    Added sample kotlin code

---
+++ src/commonTest/kotlin/dev/snipme/highlights/internal/language/KotlinTest.kt
@@ -0,0 +1,29 @@
+package dev.snipme.highlights.internal.language
+
+import dev.snipme.highlights.Highlights
+import dev.snipme.highlights.model.SyntaxLanguage
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class KotlinTest {
+
+    @Test
+    fun test() {
+        // TODO Remove "value" from comments
+        // TODO Return only valid first "val" keyword
+        val code = """
+            val intent = 0
+            copy(input = input(intent.value)) // highlights "value"
+            
+        """.trimIndent()
+
+        val result = Highlights.Builder(
+            language = SyntaxLanguage.KOTLIN,
+            code = code
+        ).build().getCodeStructure()
+
+        result.printPhrases(code)
+
+        assertEquals(1, result.keywords.size)
+    }
+}
