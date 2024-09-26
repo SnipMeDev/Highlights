64f303fef6227e99473b03d91a3541378aa7d0bd
tkadziolka <tmax0135@gmail.com>
2024-09-26 08:11:23 +0200

    Improved cancellation

--- src/commonMain/kotlin/dev/snipme/highlights/HighlightsResultListener.kt
+++ src/commonMain/kotlin/dev/snipme/highlights/HighlightsResultListener.kt
@@ -4,14 +4,14 @@ import dev.snipme.highlights.model.CodeHighlight
 
 interface HighlightsResultListener {
     fun onStart()
-    fun onComplete(highlights: List<CodeHighlight>)
+    fun onComplete(result: List<CodeHighlight>)
     fun onError(exception: Throwable)
     fun onCancel()
 }
 
 abstract class DefaultHighlightsResultListener : HighlightsResultListener {
     override fun onStart() {}
-    override fun onComplete(highlights: List<CodeHighlight>) {}
+    override fun onComplete(result: List<CodeHighlight>) {}
     override fun onError(exception: Throwable) {}
     override fun onCancel() {}
 }
