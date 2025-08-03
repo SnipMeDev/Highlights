91b7d8a4617a7c9d6a2bd283f309acf14e8a6db7
jakub-pietruch-solid <jakub.pietruch@solid.software>
2025-08-03 13:24:37 +0200

    Fix detecting independent phrase after underscore or number suffix

--- src/commonMain/kotlin/dev/snipme/highlights/internal/Extensions.kt
+++ src/commonMain/kotlin/dev/snipme/highlights/internal/Extensions.kt
@@ -1,5 +1,8 @@
 package dev.snipme.highlights.internal
 
+import dev.snipme.highlights.internal.locator.NUMBER_TYPE_CHARACTERS
+import dev.snipme.highlights.internal.SyntaxTokens.MARK_CHARACTERS
+import dev.snipme.highlights.internal.SyntaxTokens.PUNCTUATION_CHARACTERS
 import dev.snipme.highlights.model.CodeHighlight
 import dev.snipme.highlights.model.PhraseLocation
 import kotlinx.coroutines.Job
@@ -15,7 +18,7 @@ fun String.phraseLocationSetFromJson(): Set<PhraseLocation> {
     return Json.decodeFromString(this)
 }
 
-inline operator fun <E> Set<E>.get(i: Int): E? {
+operator fun <E> Set<E>.get(i: Int): E? {
     this.forEachIndexed { index, t ->
         if (i == index) return t
     }
@@ -77,18 +80,72 @@ fun String.isIndependentPhrase(
     // Token is at start of the code
     val charAfter = code[minOf(index + this.length, code.lastIndex)]
     if (index == 0) {
-        return charAfter.isDigit().not() && charAfter.isLetter().not()
+        return charAfter.isDigit().not() && charAfter.isLetter().not() && charAfter != '_'
     }
 
     // Token is at end of the code
     val charBefore = code[maxOf(index - 1, 0)]
     if (index + this.length == code.length) {
-        return charBefore.isLetter().not()
+        return (charBefore.isLetter().not() && charBefore != '_') || isAfterNumericSuffix(code, index)
     }
 
     // Token is in the middle of the code
-    return charBefore.isLetter().not() &&
-            charAfter.isDigit().not() && charAfter.isLetter().not()
+    return ((charBefore.isLetter().not() || isAfterNumericSuffix(code, index)) &&
+            charAfter.isDigit().not() && charAfter.isLetter().not() && charAfter != '_')
+}
+
+private fun String.isAfterNumericSuffix(code: String, keywordIndex: Int): Boolean {
+    if (keywordIndex == 0) return false
+
+    val charBefore = code[keywordIndex - 1]
+
+    // Check if the character before is a valid numeric suffix
+    val validSuffixes = NUMBER_TYPE_CHARACTERS
+    if (charBefore !in validSuffixes) return false
+
+    // Walk backwards to validate the number structure
+    var i = keywordIndex - 2
+    var hasDigit = false
+    var hasDot = false
+
+    while (i >= 0) {
+        val char = code[i]
+
+        when {
+            char.isDigit() -> {
+                hasDigit = true
+                i--
+            }
+            char == '.' -> {
+                if (hasDot) return false
+                hasDot = true
+                i--
+            }
+            char == '-' -> {
+                if (i == 0) {
+                    break
+                } else {
+                    val prevChar = code[i - 1]
+                    if (prevChar.isLetterOrDigit() || prevChar == '_') {
+                        return false
+                    } else {
+                        break
+                    }
+                }
+            }
+            char == '_' -> {
+                i--
+            }
+            char.isLetter() -> {
+                return false
+            }
+            else -> {
+                break
+            }
+        }
+    }
+
+    return hasDigit
 }
 
 fun Set<PhraseLocation>.toRangeSet(): Set<IntRange> =
